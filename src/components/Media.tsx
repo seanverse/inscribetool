bb0f7898326d3760a687124733e7c6c962ce7611
seanx <seanrush@163.com>
2023-12-19 13:22:59 +0800

    + add new code

---
+++ src/components/Media.tsx
@@ -0,0 +1,43 @@
+import { Box } from "@mui/material";
+import Link from "next/link";
+
+export default function Media() {
+  const mediaList = [
+    {
+      title: "代码",
+      linkText: "Github",
+      link: "https://github.com/"
+    }
+  ];
+
+  return (
+    <div className=" py-4">
+      <div className=" flex items-center justify-center gap-x-4 max-sm:flex-col">
+        {mediaList.map(({ title, linkText, link }) => {
+          return (
+            <div
+              key={title}
+              className=" flex items-center gap-2 text-xl"
+            >
+              <span>{title}:</span>
+              <Box
+                component={Link}
+                href={link}
+                className=" hover:underline"
+                sx={{
+                  color: "primary.main",
+                }}
+              >
+                {linkText}
+              </Box>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className=" text-center">
+        打赏地址☕️: 0xa2d596fbd6a4503d299657f33e7a1d2e605b30c3
+      </div>
+    </div >
+  );
+}
